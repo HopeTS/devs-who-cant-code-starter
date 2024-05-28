406b9aee214de787f7e76db796efd4f6847271cc
Robby <bergersr97@gmail.com>
2024-05-28 11:38:23 -0400

    devs who can't code starting point

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-import { versions, config } from 'pear'
-import { html } from 'htm/react'
-import { createRoot } from 'react-dom/client'
-import { UserProvider } from './src/context/user'
-import { PeersProvider } from './src/context/peers'
-import App from './src/containers/App'
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
+import { versions, config } from "pear";
+import { html } from "htm/react";
+import { createRoot } from "react-dom/client";
+import { UserProvider } from "./src/context/user";
+import App from "./src/containers/App";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 
-const { app } = await versions()
+const { app } = await versions();
 const theme = createTheme({
   palette: {
-    mode: 'dark'
-  }
-})
+    mode: "dark",
+  },
+});
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"));
 root.render(html`
   <${ThemeProvider} theme=${theme}>
     <${CssBaseline} />
     <${UserProvider} config=${config}>
-      <${PeersProvider}
-        name="filesharing-app-example"
-        topic=${app.key || '57337a386673415371314f315a6d386f504576774259624e32446a7377393752'}
-      >
-        <${App}
-          app={app}
-        />
-      </>
-    </>
-  </>
-`)
+      <${App} app="{app}" />
+    </${UserProvider}>
+  </${ThemeProvider}>
+`);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,46 @@
-import { html } from 'htm/react'
-import { useState, useEffect } from 'react'
-import useUser from '../hooks/use-user'
-import usePeers from '../hooks/use-peers'
-import ControlPanel from './ControlPanel'
-import PeersPanel from './PeersPanel'
-import UserPanel from './UserPanel'
-import {
-  Box,
-  CircularProgress,
-  Grid,
-  Typography
-} from '@mui/material'
+import { html } from "htm/react";
+import { useState, useEffect } from "react";
+import useUser from "../hooks/use-user";
+import { Box, CircularProgress, Typography } from "@mui/material";
 
 export default ({ app }) => {
-  const [isLoaded, setIsLoaded] = useState(false)
-  const { loaded: arePeersLoaded } = usePeers()
-  const { loaded: isUserLoaded } = useUser()
+  const [isLoaded, setIsLoaded] = useState(false);
+  const { loaded: isUserLoaded } = useUser();
 
   useEffect(() => {
-    const isAllLoaded = arePeersLoaded && isUserLoaded
-    if (!isAllLoaded) return
-    setIsLoaded(true)
-  }, [arePeersLoaded, isUserLoaded])
+    const isAllLoaded = isUserLoaded;
+    if (!isAllLoaded) return;
+    setIsLoaded(true);
+  }, [isUserLoaded]);
 
   if (!isLoaded) {
     return html`
       <${Box}
         sx=${{
-          display: 'flex',
-          height: '100vh',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'
+          display: "flex",
+          height: "100vh",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <${CircularProgress} sx=${{ marginBottom: '20px' }}/>
-        <${Typography} variant="h3">
-          Loading
-        </>
-      </>
-    `
+        <${CircularProgress} sx=${{ marginBottom: "20px" }} />
+        <${Typography} variant="h3"> Loading </${Typography}>
+      </${Box}>
+    `;
   }
   return html`
-    <${Grid} container spacing=${2}>
-      <${Grid} item xs=${12}>
-        <${ControlPanel} />
-      </>
-      <${Grid} item xs=${6}>
-        <${PeersPanel} />
-      </>
-      <${Grid} item xs=${6}>
-        <${UserPanel} />
-      </>
-    </>
-  `
-}
+    <${Box} sx=${{ p: 4 }}>
+      <p>
+        Welcome to the starting point for Devs Who Can't Code! This is a simple 
+        app that doesn't do anything yet. Use it as a starting point for your 
+        project.
+        <br />
+        Find on 
+        <a href="https://github.com/HopeTS/devs-who-cant-code-starter.git">
+          GitHub
+        </a>.
+      </p>
+    </${Box}>
+  `;
+};
